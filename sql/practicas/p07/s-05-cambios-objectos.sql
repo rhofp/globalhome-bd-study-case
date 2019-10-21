0ba81f0c0186d4386e187bbe80b76669a1a1edec
rhofp <rhodfra@gmail.com>
2019-10-21 06:55:22 -0500

    Solo falta modificar la pk

--- sql/practicas/p07/s-05-cambios-objectos.sql
+++ sql/practicas/p07/s-05-cambios-objectos.sql
@@ -5,7 +5,7 @@
 prompt Conectandose al usuario rfp_p0703_admin:
 connect rfp_p0703_admin/bases123
 
-alter table cuenta add curp number(18,0);
+alter table cuenta add curp varchar2(18);
 alter table cuenta_inversion add constraint cta_inversion_porcentaje_interes_chk 
 	check( porcentaje_interes <= 20.00 );
 alter table cuenta drop constraint cta_es_ahorro_o_es_inv_chk;
@@ -17,17 +17,25 @@ alter table cuenta add constraint cta_tipo_cuenta_chk
 	check( tipo_cuenta in ('I','A'));
 alter table cuenta rename column titular to nombre_titular;
 alter table movimiento_cuenta rename to operacion_cuenta;
-
+------------------ NOQUEDA LO DE LA PK -------------------------------------
 alter table operacion_cuenta rename 
 	constraint movimiento_cuenta_pk to operacion_cuenta_pk;
-alter table operacion_cuenta add operacion_cuenta_id number(12,0);
+alter table operacion_cuenta add operacion_cuenta_id number(18,0);
 alter table operacion_cuenta modify constraint operacion_cuenta_pk 
 	primary key(operacion_cuenta_id);	
 -- agregar uk a cuenta_id y a num_movimiento	
+----------------------------------------------------------------------------
 
 alter table operacion_cuenta modify importe number(20,2);
-alter view v_cuenta_movimiento rename column nombre_titular to cuentahabiente;
--- checar si exite nombre_titular en la vista
---falta 9
-alter sequence cycle minvalue 15 maxvalue 100000000;
+
+prompt Recreando vista v_cuenta_movimiento
+create or replace view v_cuenta_movimiento(
+	cuentahabiente,num_cuenta, num_movimiento, importe, concepto, 
+	tipo_movimiento, fecha_baja
+) as select nombre_titular,num_cuenta, num_movimiento, importe, concepto, 
+	tipo_movimiento, fecha_baja
+from cuenta c, operacion_cuenta oc
+where c.cuenta_id = oc.cuenta_id;
+
+alter sequence seq_cuenta cycle minvalue 15 maxvalue 100000000;
 alter index cta_rfc_titular_iuk rename to cuenta_cuentahabiente_uk;
