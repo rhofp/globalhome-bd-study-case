6acb013cb04ff9d92f4f8576510123ddd46e1570
rhofp <rhodfra@gmail.com>
2019-11-06 11:47:08 -0600

    consulta 11 rectificada

--- sql/practicas/p09/practica-compl/buf.sql
+++ sql/practicas/p09/practica-compl/buf.sql
@@ -10,16 +10,19 @@ connect kfrf_p0903_fx/practica9
 --col nombre format a10
 --col municipio format a10
 --col ultima_revision format a40
-col CLAVE_TIPO format a10
-
---CONSULTA 7
-select decode(
-	tipo,	
-	'seaplane_base','B',
-	'medium_airport','M',
-	'small_airport','S',
-	'closed','C',
-	'large_airport','L',
-	'heliport', 'H'
-) "CLAVE_TIPO", upper(tipo) "TIPO"
-from aeropuerto;
+--col CLAVE_TIPO format a10
+col path format a15
+-- CONSULTA 11
+select nombre, municipio,path,
+nvl(length(nombre),0)+
+nvl(length(municipio),0)+
+nvl(length(path),0) total_longitud from (
+	select nombre, municipio, 
+	substr(
+		wikipedia_link, 
+		instr(wikipedia_link,'/',1,4),
+		length(wikipedia_link)-1
+	) path
+	from aeropuerto 
+	where tipo='small_airport' and region_iso='MX-BCS'
+);
