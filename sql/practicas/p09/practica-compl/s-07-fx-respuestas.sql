12c9892cd6da977f6c41f78de3930b447a095189
rhofp <rhodfra@gmail.com>
2019-11-07 08:22:32 -0600

    COnsulta 5, lista

--- sql/practicas/p09/practica-compl/s-07-fx-respuestas.sql
+++ sql/practicas/p09/practica-compl/s-07-fx-respuestas.sql
@@ -48,13 +48,8 @@ where region_iso='MX-OAX'
 
 -- CONSULTA 5
 create table consulta_5 as(
-select id,clave,nombre,municipio,codigo_gps, codigo_iata, 
-trim(to_char(ultima_revision,'day' ))||', '||
-trim(to_char(ultima_revision,'month'))||' '|| 
-trim(to_char(ultima_revision,'dd'))||' of ' ||
-trim(to_char(ultima_revision,'yyyy'))||' at '||
-trim(to_char(ultima_revision,'hh24:mi:ss'))
-"ULTIMA_REVISION"
+select id,clave,nombre,municipio,codigo_gps, codigo_iata,
+to_char(ultima_revision,'FMday, month dd "of" yyyy "at" hh24:mi:ss') "ULTIMA_REVISION"
 from aeropuerto
 where region_iso='MX-CHP'
 );
