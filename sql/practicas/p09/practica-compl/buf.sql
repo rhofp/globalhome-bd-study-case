577a7ac1f7fa50841afc46fedf81d6919c0149d6
rhofp <rhodfra@gmail.com>
2019-11-07 01:27:43 -0600

    agrego buf

--- sql/practicas/p09/practica-compl/buf.sql
+++ sql/practicas/p09/practica-compl/buf.sql
@@ -6,23 +6,21 @@
 prompt Conectando como el usuario kfrf_p0903_fx 
 connect kfrf_p0903_fx/practica9
 
-col path format a20
-col nombre format a10
-col municipio format a10
-col ultima_revision format a30
+--col path format a20
+--col nombre format a10
+--col municipio format a10
+col ultima_revision format a20
 --col CLAVE_TIPO format a10
-col path format a15
--- CONSULTA 11
-select nombre, municipio,path,
-nvl(length(nombre),0)+
-nvl(length(municipio),0)+
-nvl(length(path),0) total_longitud from (
-	select nombre, municipio, 
-	substr(
-		wikipedia_link, 
-		instr(wikipedia_link,'/',1,4),
-		length(wikipedia_link)-1
-	) path
-	from aeropuerto 
-	where tipo='small_airport' and region_iso='MX-BCS'
-);
+--col path format a15
+col tipo format a10
+
+select id,nombre,clave,municipio,tipo,
+to_char(ultima_revision,'dd/mm/yyyy hh:mi:ss')||' hrs.' "ULTIMA_REVISION"
+from aeropuerto where 
+	ultima_revision>=to_date('08/2012','mm/yyyy') and	
+	ultima_revision<=to_date('03/2015','mm/yyyy')
+--intersect
+--select id,nombre,clave,municipio,tipo,
+--to_char(ultima_revision,'dd/mm/yyyy hh:mi:ss')||' hrs.' "ULTIMA_REVISION"
+--from aeropuerto where 
+and tipo='closed';
