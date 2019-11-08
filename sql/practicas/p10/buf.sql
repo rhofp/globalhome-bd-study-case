cfcb5d9948461d528e7601c85d4799b9b9232083
rhofp <rhodfra@gmail.com>
2019-11-08 14:23:32 -0600

    Agrego consulta 3 p10

--- sql/practicas/p10/buf.sql
+++ sql/practicas/p10/buf.sql
@@ -6,7 +6,27 @@
 prompt Conectando como el usuario flfr_p1001_subastas 
 connect flfr_p1001_subastas/practica10
 
+--Empleando natural join, generar un reporte que muestre el 
+--id, nombre, precio inicial, precio de venta y tipo de artículo, 
+--así como el nombre y fecha inicio de la subasta 
+--de todos los artículos que compró el 
+--cliente MARICELA PAEZ MARTINEZ durante 
+--las subastas realizadas en el 2010. 
+--Nota: Un artículo se considera comprado cuando se registra el cliente
+--que lo adquirió y se registra el precio de venta.
 
+col NOMBRE format a5
+col SUBASTA_NOMBRE format a5
 
+select a.articulo_id,a.nombre "NOMBRE_ARTICULO",a.precio_inicial,sv.precio_venta,
+a.tipo_articulo,s.nombre "NOMBRE_SUBASTA",s.fecha_inicio
+from articulo a,subasta_venta sv,subasta s,cliente c
+where a.articulo_id=sv.articulo_id
+and a.subasta_id=s.subasta_id
+and c.cliente_id=sv.cliente_id
+and c.nombre='MARICELA' 
+and c.apellido_paterno='PAEZ' 
+and c.apellido_materno='MARTINEZ'
+and to_char(s.fecha_inicio,'yyyy')='2010';
 
 
--- sql/practicas/p10/s-04-respuestas.sql
+++ sql/practicas/p10/s-04-respuestas.sql
@@ -30,3 +30,27 @@ create table consulta_2 as(
 	join pais p using (pais_id)
 	where sa.clave='ENTREGADO' and p.descripcion='BELGICA'
 );
+
+-- CONSULTA 3
+--Empleando natural join, generar un reporte que muestre el 
+--id, nombre, precio inicial, precio de venta y tipo de artículo, 
+--así como el nombre y fecha inicio de la subasta 
+--de todos los artículos que compró el 
+--cliente MARICELA PAEZ MARTINEZ durante 
+--las subastas realizadas en el 2010. 
+--Nota: Un artículo se considera comprado cuando se registra el cliente
+--que lo adquirió y se registra el precio de venta.
+
+create table consulta_3 as(
+	select a.articulo_id, a.nombre, a.precio_inicial,sv.precio_venta,
+	a.tipo_articulo,s.nombre "SUBASTA_NOMBRE",
+	to_char(s.fecha_inicio,'yyyy/mm/dd hh:mi:ss AM') "FECHA_INICIO"
+	from articulo a,subasta_venta sv,subasta s,cliente c
+	where a.articulo_id=sv.articulo_id
+	and a.subasta_id=s.subasta_id
+	and c.cliente_id=sv.cliente_id
+	and c.nombre='MARICELA' 
+	and c.apellido_paterno='PAEZ' 
+	and c.apellido_materno='MARTINEZ'
+	and to_char(s.fecha_inicio,'yyyy')='2010'
+);
