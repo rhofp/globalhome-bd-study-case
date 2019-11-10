d22c4bd9c6ae812f81c3de3ea09ca436b12937b7
rhofp <rhodfra@gmail.com>
2019-11-09 19:58:21 -0600

    A la C10 si le puso odio

--- sql/practicas/p10/buf.sql
+++ sql/practicas/p10/buf.sql
@@ -8,25 +8,35 @@ connect flfr_p1001_subastas/practica10
 
 --col nombre format a8
 --col clave format a10
-col descripcion format a20
+--col descripcion format a20
 -- col apellido_paterno format a10
 -- col apellido_materno format a10
 -- col tipo_tarjeta format a10
 -- col email format a10
 -- col ocupacion format a10
 
---CONSULTA 9
---Suponga que un cliente decide realizar una consulta en el catálogo de artículos. 
---El cliente está interesado por todos aquellos artículos cuyo 
---nombre o descripción hagan referencia o hablen de la palabra “Colonial” 
---que no han sido aún vendidos, y que el articulo este en proceso de ser subastado.
---Generar la sentencia SQL que muestre el nombre 
---y tipo de todos los artículos que cumplan con los criterios de búsqueda.
---R: Se debe obtener 1 registro.
+--CONSULTA 10
+--Suponga que se desea generar un reporte a detalle de 
+--todos los artículos que fueron comprados y pagados con la tarjeta de crédito
+--5681375824866375.Los datos que el reporte debe mostrar son los siguientes:
+-- a. Fecha de la factura
+-- b. Numero de la tarjeta
+-- c. Nombre y apellidos del cliente
+-- d. Precio de venta de cada articulo
+-- e. Precio de compra de cada articulo
+-- f. Diferencia entre el precio de compra y el de venta
+-- g. Nombre y clave del artículo
+-- h. Tipo de artículo
+-- i. Nombre completo del famoso al que perteneció el articulo (
+---en caso de ser articulo perteneciente a un famoso)
+-- j. Año de hallazgo, en caso de que el artículo sea arqueológico.
+-- k. Clave del país, en caso de que el articulo haya sido donado por dicho país.
 
-select a.nombre,a.tipo_articulo,a.descripcion
-from articulo a, status_articulo sa
-where a.status_articulo_id=sa.status_articulo_id
-and sa.clave = 'EN SUBASTA'
-and a.nombre like '%Colonial%' 
-or a.descripcion like '%Colonial%';
+select fc.fecha_factura,tc.numero_tarjeta,c.nombre,c.apellido_paterno,
+c.apellido_materno,sv.precio_venta, a.precio_inicial, 
+a.precio_inicial-sv.precio_venta as diferencia,
+a.nombre,a.clave_articulo,a.tipo_articulo, af.nombre_completo,aa.anio_hallazgo,
+p.clave
+from cliente c
+join tarjeta_cliente tc
+on c.cliente_id=tc.cliente_id
