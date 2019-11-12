c4d2891bc96ed8d666b657554c6cd288f32638c6
rhofp <rhodfra@gmail.com>
2019-11-12 11:01:31 -0600

    ej2 p12 compila sin errores, falta completar último pago

--- sql/practicas/p12/s-04-ejercicio-pago-inmuebles.sql
+++ sql/practicas/p12/s-04-ejercicio-pago-inmuebles.sql
@@ -3,6 +3,8 @@
 --@Fecha creación: 04/11/2019
 --@Descripcion: Script encargado de validar e insertar pagos
 
+connect fgfp_p1203_inmuebles/practica12
+
 set serveroutput on
 create or replace procedure sp_corrige_pago_inmuebles(p_inmueble_id number,
 p_actualiado out number) is
@@ -13,6 +15,7 @@ v_num_pagos_contador number;
 v_primer_fecha_pago date;
 v_precio_venta compra_inmueble.precio_venta%type;
 v_precio_importe pago_inmueble.importe%type;
+v_fecha_status date;
 
 begin
 
@@ -36,27 +39,27 @@ begin
 
 		-- 1. Eliminar los pagos existentes del inmueble_id
 		delete from pago_inmueble 
-		where inmueble_id=p_inmueble_id;
+		where inmueble_id = p_inmueble_id;
 		-- 2. Calcular la primera fecha de pago con 
 		--		add_months(fecha_status,-1*(plazo-1))
-		select add_months(fecha_status,-1*(plazo-1)) into v_primer_fecha_pago 
+		select add_months(fecha_status,-1*(v_plazo-1)) into v_primer_fecha_pago 
 		from inmueble,compra_inmueble;
 		-- 3. Calcular el importe por mes
-		select precio_venta/plazo into v_precio_importe from compra_inmueble;
+		select precio_venta/v_plazo into v_precio_importe from compra_inmueble;
 		-- 4. Iterar desde 1 hasta plazo y hacer los inserts necesarios.
-		for v_num_pagos_contador in 1 .. plazo loop
-			insert into pago_inmueble(inmueble_id,num_pago,primer_fecha,importe)
+		for v_num_pagos_contador in 1 .. v_plazo loop
+			insert into pago_inmueble(inmueble_id,num_pago,fecha_pago,importe)
 			values(p_inmueble_id,v_num_pagos_contador,v_primer_fecha_pago,v_precio_importe);
 
-			select v_primer_fecha_pago+1 into v_primer_fecha_pago from dual;
+			v_primer_fecha_pago := v_primer_fecha_pago+1;
 		end loop;
 		-- Solo falta agregarle los centavos perdidos al último pago.
 		-- indicamos que si hubo actualizacion
-		p_actualiado = 1;
+		p_actualiado := 1;
 
 	else
 		-- si no entre al if entonce no se hacen actualizaciones, por lo tanto
-		p_actualiado = 0;
+		p_actualiado := 0;
 	end if;
 
 end;
