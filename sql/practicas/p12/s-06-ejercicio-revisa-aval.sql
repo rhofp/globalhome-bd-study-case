a426f2376cca25cc0e670e7a21a82472adfcbe9a
rhofp <rhodfra@gmail.com>
2019-11-13 13:32:55 -0600

    Agrego p12 ej3

--- sql/practicas/p12/s-06-ejercicio-revisa-aval.sql
+++ sql/practicas/p12/s-06-ejercicio-revisa-aval.sql
@@ -3,14 +3,17 @@
 --@Fecha creación: 04/11/2019
 --@Descripcion: Script encargado de crear trigger
 
+connect fgfp_p1203_inmuebles/practica12
+
 set serveroutput on
 
 create or replace trigger revision_avales
 	-- el triger se activa en insercion o actualizacion de cliente_id y status_inm...
 before insert or update on inmueble
+for each row
 declare
 	v_aval_cliente_id cliente.cliente_id%type;
-	--v_status_inmueble_id inmueble.status_inmueble_id%type;
+	v_status_inmueble_id inmueble.status_inmueble_id%type;
 begin
 	
 	if :new.cliente_id is null
@@ -26,26 +29,29 @@ begin
 	end if;	
 
 	if :new.cliente_id is not null
-	and  :new.status_inmueble_id <>1 
-	and :new.tatus_inmueble_id <> 5 then
+	and :new.status_inmueble_id <> 1 
+	and :new.status_inmueble_id <> 5 then
 		-- checar que el nuevo cliente cuente con aval asignado.
 		select aval_cliente_id into v_aval_cliente_id
 		from cliente where cliente_id = :new.cliente_id;
 
 		-- si hay aval, debe contar con al menos un inmueble pagado
 		if v_aval_cliente_id is not null then
-			--select status_inmueble_id into v_status_inmueble_id from inmueble
-			--where status_inmueble_id = 5 and cliente_id = v_aval_cliente_id;
+			select status_inmueble_id into v_status_inmueble_id from inmueble
+			where status_inmueble_id = 5 and cliente_id = v_aval_cliente_id;
 
-			if :new.v_status_inmueble_id = 5 then
+			if :new.status_inmueble_id = 5 then
 				insert into asignacion_pendiente(asignacion_pendiente_id,
 				descripcion,fecha_registro,inmueble_id,cliente_sin_aval_id)
-				values(seq_asignacion_pendiente,'El cliente no cuenta con un aval válido',
+				values(seq_asignacion_pendiente.nextval,
+				'El cliente no cuenta con un aval válido',
 				sysdate,:new.inmueble_id,:new.cliente_id);
 			end if;
 
-		-- en caso contrario, registrar al cliente en asignacion_pendiente y posteriomente lanzar
-		-- error
+		-- en caso contrario, registrar al cliente en asignacion_pendiente 
+		--y posteriomente lanzar error
 		end if;
+	end if;
 end;
 /
+show errors;
