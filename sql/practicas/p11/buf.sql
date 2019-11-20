8892d3bbe0986b2aac2e81ca5e95f842a3038b99
rhofp <rhodfra@gmail.com>
2019-11-20 16:13:22 -0600

    Agrego consulta 13

--- sql/practicas/p11/buf.sql
+++ sql/practicas/p11/buf.sql
@@ -1,27 +1,18 @@
 -- col lugar format a10
 col nombre_articulo format a10
--- col nombre format a10
+col nombre format a10
 -- col nombre_subasta format a10
 -- col email format a10
 
 connect flfr_p1101_subastas/david
 
---tablas involucradas: subasta,articulo
-select s.subasta_id,fecha_inicio,articulo_id,a.nombre nombre_articulo,
-precio_inicial,(
-	select avg(precio_inicial) 
-	from articulo a
-	join subasta s
-	on s.subasta_id = a.subasta_id
-	where lower(a.nombre) like '%motocicleta%'
-	and extract(year from fecha_inicio) = 2010
-	and extract(year from fecha_fin) = 2010
-) promedio
-from subasta s, articulo a
+--consulta 13
+select s.subasta_id,s.nombre,s.fecha_inicio,sum(sv.precio_venta) total_ventas
+from subasta s, articulo a, subasta_venta sv
 where s.subasta_id = a.subasta_id
+and a.articulo_id = sv.articulo_id
 and extract(year from fecha_inicio) = 2010
 and extract(year from fecha_fin) = 2010
-and extract(month from fecha_inicio) = 7
-and lower(a.nombre) like '%motocicleta%'
-and (status_articulo_id = 3
-or status_articulo_id = 4);
+group by s.subasta_id,s.nombre,s.fecha_inicio
+having sum(sv.precio_venta) >= 3000000
+order by s.subasta_id;
--- sql/practicas/p11/s-02-respuestas.sql
+++ sql/practicas/p11/s-02-respuestas.sql
@@ -172,3 +172,15 @@ create table consulta_12 as(
 		having count(a.articulo_id)>=3
 	)
 );
+--EJERCICIO 13
+create table consulta_13 as(
+	select s.subasta_id,s.nombre,s.fecha_inicio,
+	sum(sv.precio_venta) total_ventas
+	from subasta s, articulo a, subasta_venta sv
+	where s.subasta_id = a.subasta_id
+	and a.articulo_id = sv.articulo_id
+	and extract(year from fecha_inicio) = 2010
+	and extract(year from fecha_fin) = 2010
+	group by s.subasta_id,s.nombre,s.fecha_inicio
+	having sum(sv.precio_venta) >= 3000000
+);
