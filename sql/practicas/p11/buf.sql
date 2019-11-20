7da4e0e9ffb7a516ba75717f52c191d242929b94
rhofp <rhodfra@gmail.com>
2019-11-19 18:03:00 -0600

    Agrego consulta 1,3,4,6,8 de P11

--- sql/practicas/p11/buf.sql
+++ sql/practicas/p11/buf.sql
@@ -7,27 +7,59 @@ col email format a10
 
 connect flfr_p1101_subastas/david
 
---EJERCICIO 8 (ya no la pude optimizar más :( )
-select q1.subasta_id, s.nombre,s.fecha_inicio,a.nombre,a.clave_articulo,
-max(q1.precio_venta) mas_caro from (
-	select s.subasta_id,sv.precio_venta
+-- select q1.subasta_id, s.nombre nombre_subasta,s.fecha_inicio,
+-- a.nombre nombre_articulo,a.clave_articulo,
+-- max(q1.precio_venta) mas_caro from (
+-- 	select s.subasta_id,sv.precio_venta,a.nombre nombre_articulo
+-- 	from subasta s,articulo a, subasta_venta sv
+-- 	where s.subasta_id = a.subasta_id 
+-- 	and sv.articulo_id = a.articulo_id
+-- 	and (
+-- 		extract(year from fecha_inicio) = 2010
+-- 		and extract(year from fecha_fin) = 2010
+-- 	) and (
+-- 		extract(month from fecha_inicio) = 1
+-- 		or extract(month from fecha_inicio) = 3
+-- 		or extract(month from fecha_inicio) = 6
+-- 	) and (
+-- 		a.status_articulo_id = 3 
+-- 		or a.status_articulo_id = 4
+-- 	)
+-- ) q1
+-- join subasta s
+-- on q1.subasta_id = s.subasta_id
+-- join articulo a
+-- on a.subasta_id = q1.subasta_id
+-- join subasta_venta sv
+-- on sv.articulo_id = a.articulo_id
+-- where q1.nombre_articulo = a.nombre
+-- group by  q1.subasta_id, s.nombre,s.fecha_inicio,sv.precio_venta,
+-- a.nombre,a.clave_articulo
+-- having max(q1.precio_venta) = sv.precio_venta
+-- order by q1.subasta_id;
+
+select q1.subasta_id,q1.nombre_subasta,fecha_inicio,a.nombre nombre_articulo,
+a.clave_articulo,q1.mas_caro
+from (
+	select s.subasta_id,max(precio_venta) mas_caro,fecha_inicio,fecha_fin,
+	s.nombre nombre_subasta
 	from subasta s,articulo a, subasta_venta sv
-	where s.subasta_id = a.subasta_id and sv.articulo_id = a.articulo_id
-	and (
-		extract(year from fecha_inicio) = 2010
-		and extract(year from fecha_fin) = 2010
-	) and (
-		extract(month from fecha_inicio) = 1
-		or extract(month from fecha_inicio) = 3
-		or extract(month from fecha_inicio) = 6
-	) and (
-		a.status_articulo_id = 3 
-		or a.status_articulo_id = 4
-	)
-) q1
-join subasta s
-on q1.subasta_id = s.subasta_id
-join articulo a
-on a.articulo_id = s.subasta_id
-group by  q1.subasta_id, s.nombre,s.fecha_inicio,a.nombre,a.clave_articulo
-order by q1.subasta_id;
+	where s.subasta_id = a.subasta_id 
+	and sv.articulo_id = a.articulo_id
+	group by s.subasta_id,fecha_inicio,fecha_fin,s.nombre
+) q1,articulo a, subasta_venta sv
+where q1.subasta_id = a.subasta_id 
+and sv.articulo_id = a.articulo_id
+and sv.precio_venta = q1.mas_caro
+and (
+	extract(year from fecha_inicio) = 2010
+	and extract(year from fecha_fin) = 2010
+) and (
+	extract(month from fecha_inicio) = 1
+	or extract(month from fecha_inicio) = 3
+	or extract(month from fecha_inicio) = 6
+) and (
+	a.status_articulo_id = 3 
+	or a.status_articulo_id = 4
+)
+order by subasta_id;
--- sql/practicas/p11/s-02-respuestas.sql
+++ sql/practicas/p11/s-02-respuestas.sql
@@ -1,4 +1,9 @@
+--@Autor: Flores García Karina
+--@Autor: Pablo Rodrigo Francisco
+--@Fecha creación: 19/11/2019
+--@Descripcion: Subconsultas
 
+--EJERCICIO 1
 create table consulta_1 as (
 	select count(*) num_articulos, sum(precio_venta) ingresos from (
 		select precio_venta
@@ -12,6 +17,7 @@ create table consulta_1 as (
 	)
 );
 
+--EJERCICIO 3
 create table consulta_3 as ( -- totalmente copiada de David
 	select min(precio_inicial) mas_barato_compra, 
 	max(precio_inicial) mas_caro_compra, 
@@ -28,6 +34,7 @@ create table consulta_3 as ( -- totalmente copiada de David
 	)
 );
 
+--EJERCICIO 4
 create table consulta_4 as (
 	select q1.cliente_id,email,numero_tarjeta from (
 		select c.cliente_id
@@ -42,6 +49,7 @@ create table consulta_4 as (
 	and c.cliente_id = q1.cliente_id
 );
 
+--EJERCICIO 6
 create table consulta_6 as
 	select s.nombre, s.fecha_inicio, s.lugar, a.tipo_articulo, 
 	sum(sv.precio_venta) as total_venta
@@ -54,3 +62,31 @@ create table consulta_6 as
 	      to_char(s.fecha_fin, 'YYYY') = '2009'
 	group by s.nombre, s.fecha_inicio, s.lugar, a.tipo_articulo
 	order by total_venta desc;
+
+--EJERCICIO 8 (ya no la pude optimizar más :( )
+create table consulta_8 as
+	select q1.subasta_id,q1.nombre_subasta,fecha_inicio,
+	a.nombre nombre_articulo,a.clave_articulo,q1.mas_caro
+	from (
+		select s.subasta_id,max(precio_venta) mas_caro,
+		fecha_inicio,fecha_fin,	s.nombre nombre_subasta
+		from subasta s,articulo a, subasta_venta sv
+		where s.subasta_id = a.subasta_id 
+		and sv.articulo_id = a.articulo_id
+		group by s.subasta_id,fecha_inicio,fecha_fin,s.nombre
+	) q1,articulo a, subasta_venta sv
+	where q1.subasta_id = a.subasta_id 
+	and sv.articulo_id = a.articulo_id
+	and sv.precio_venta = q1.mas_caro
+	and (
+		extract(year from fecha_inicio) = 2010
+		and extract(year from fecha_fin) = 2010
+	) and (
+		extract(month from fecha_inicio) = 1
+		or extract(month from fecha_inicio) = 3
+		or extract(month from fecha_inicio) = 6
+	) and (
+		a.status_articulo_id = 3 
+		or a.status_articulo_id = 4
+	)
+	order by subasta_id;
