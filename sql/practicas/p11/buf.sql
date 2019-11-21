db983ae7ba1a456e24b9ed0282ce14da3e5b5f84
rhofp <rhodfra@gmail.com>
2019-11-21 13:22:03 -0600

    Agrego consulta 9

--- sql/practicas/p11/buf.sql
+++ sql/practicas/p11/buf.sql
@@ -26,25 +26,22 @@ connect flfr_p1101_subastas/david
 -- 		from factura_cliente
 -- 	)
 -- );
-
-select fc.factura_cliente_id
+select sum(precio_venta) total
 from factura_cliente fc, subasta_venta sv, cliente c
 where fc.factura_cliente_id = sv.factura_cliente_id
 and c.cliente_id = sv.cliente_id
 and c.nombre = 'GALILEA'
 and c.apellido_paterno = 'GOMEZ'
 and c.apellido_materno = 'GONZALEZ'
-and fecha_factura = (
-	select max(max_fecha) from (
-		select fc.factura_cliente_id,max(fecha_factura) max_fecha
-		from factura_cliente fc, subasta_venta sv, cliente c
-		where fc.factura_cliente_id = sv.factura_cliente_id
-		and c.cliente_id = sv.cliente_id
-		and c.nombre = 'GALILEA'
-		and c.apellido_paterno = 'GOMEZ'
-		and c.apellido_materno = 'GONZALEZ'
-		group by fc.factura_cliente_id,fecha_factura
-	)
-);
+and fecha_factura =(
+	select max(fecha_factura) max_fecha
+	from factura_cliente fc, subasta_venta sv, cliente c
+	where fc.factura_cliente_id = sv.factura_cliente_id
+	and c.cliente_id = sv.cliente_id
+	and c.nombre = 'GALILEA'
+	and c.apellido_paterno = 'GOMEZ'
+	and c.apellido_materno = 'GONZALEZ'
+)
+group by fecha_factura,sv.cliente_id;
 
 
