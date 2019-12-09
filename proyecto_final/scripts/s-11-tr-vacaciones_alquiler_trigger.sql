3379f3ce495247ae828c6f0087bbb15ecece26ea
rhofp <rhodfra@gmail.com>
2019-12-09 12:54:48 -0600

    Agrego trigger de vivienda

--- proyecto_final/scripts/s-11-tr-vacaciones_alquiler_trigger.sql
+++ proyecto_final/scripts/s-11-tr-vacaciones_alquiler_trigger.sql
@@ -5,20 +5,14 @@
 create or replace trigger vacaciones_alquiler_trigger
 after insert on ALQUILER
 FOR EACH ROW
+
 DECLARE
-	Vtarjeta tarjeta.tarjeta_id%ROWTYPE;
-	VnuevaClabe tarjeta.CLABE%ROWTYPE;
-	VnuevoMes tarjeta.mesExp%ROWTYPE;
-	VnuevoAño tarjeta.añoExp%ROWTYPE;
+	v_tarjeta tarjeta.tarjeta_id%type;
 BEGIN
-	IF INSERTING THEN
-		SELECT tarjeta_id into Vtarjeta FROM TARJETA WHERE cliente_id = :NEW.usuario_id; 
-		IF Vtarjeta is not null then
-			DBMS_OUTPUT.PUT_LINE('Por favor inserta tu CLABE' || VnuevaClabe);
-			DBMS_OUTPUT.PUT_LINE('Por favor inserta el mes expiración' || VnuevoMes);
-			DBMS_OUTPUT.PUT_LINE('Por favor inserta el año de expiración' || VnuevoAño);
-			insert into ALQUILER VALUES (tarjeta_seq.NEXTVAL, VnuevaClabe, :NEW.usuario_id, VnuevoMes, VnuevoAño);
-		END IF;
+	SELECT tarjeta_id into v_tarjeta 
+	FROM TARJETA WHERE cliente_id = :new.usuario_id; 
+	IF v_tarjeta is null then
+		DBMS_OUTPUT.PUT_LINE('Por favor agrega una forma de pago antes de continuar navegando.');
 	END IF;
 END;
 /
