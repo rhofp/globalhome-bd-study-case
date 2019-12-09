b96117ea317b8d9eb0327913695e569a39b4eaae
rhofp <rhodfra@gmail.com>
2019-12-09 00:14:35 -0600

    feat:agrego tabla temporal para almacenar viviendas invalidas

--- proyecto_final/scripts/s-03-tablas-temporales.sql
+++ proyecto_final/scripts/s-03-tablas-temporales.sql
@@ -2,3 +2,13 @@
 --@Autor:Francisco Pablo Rodrigo
 --@Fecha creación: 30/11/2019
 --@Descripción: Creacion de tablas temporales
+
+create global temporary table vivienda_invalida_temp (
+	  vivienda_id number(10,0),
+	  ubicacion_longitud number(10,7),
+	  ubicacion_latitud number(10,7),
+	  direccion varchar2(250),
+	  capacidad_personas_max number(2,0),
+	  descripcion varchar2(2500),
+	  tipo varchar2(50)
+) on commit preserve rows;
