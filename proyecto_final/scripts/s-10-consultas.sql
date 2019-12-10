988f4144f8a50d162131998471af8d014aae1680
KarinaFlo <karinaf141@gmail.com>
2019-12-10 14:56:18 -0600

    Agrego la primera consulta funcionando, ya la probe

--- proyecto_final/scripts/s-10-consultas.sql
+++ proyecto_final/scripts/s-10-consultas.sql
@@ -4,18 +4,36 @@
 --@Descripción: Consultas
 
 --Mostrar los datos del servicio más común en las viviendas anunciadas. (Uso de join, funciones de agregación, subconsulta).
-select servicio_id,nombre,descripción,max(total_viviendas)
-from select servicio_id,count(vivienda_id) as total_viviendas
-  from servicio_vivienda
-  group by servicio_id
- group by servicio_id,nombre,descripción
- having max(total_viviendas);
-
-
-
+select servicio_id,nombre,descripcion,total_viviendas
+from (
+select s.servicio_id,s.nombre,s.descripcion,count(sv.vivienda_id) as total_viviendas
+from servicio s
+join servicio_vivienda sv
+on s.servicio_id = sv.servicio_id
+group by s.servicio_id,s.nombre,s.descripcion
+) where total_viviendas=(
 select max(total_viviendas)from(
 select s.servicio_id,count(sv.vivienda_id) as total_viviendas
 from servicio s
 join servicio_vivienda sv
 on s.servicio_id = sv.servicio_id
-group by s.servicio_id);
+group by s.servicio_id)
+);
+
+--Esta parte extrae el mayor nmero de viviendas para todos los servicios
+--es decir cual es el servicio mas consumido
+--select max(total_viviendas)from(
+--select s.servicio_id,count(sv.vivienda_id) as total_viviendas
+--from servicio s
+--join servicio_vivienda sv
+--on s.servicio_id = sv.servicio_id
+--group by s.servicio_id);
+
+--Esta parte hace el conteo de viviendas por servicio
+--select s.servicio_id,s.nombre,count(sv.vivienda_id) "total_viviendas"
+--from servicio s
+--join servicio_vivienda sv
+--on s.servicio_id = sv.servicio_id
+--group by s.servicio_id,s.nombre;
+
+
