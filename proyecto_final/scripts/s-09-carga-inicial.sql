d032051879ad8fc0ac306ca01834be0600ef5f75
rhofp <rhodfra@gmail.com>
2019-12-10 14:10:42 -0600

    Casi listo archivo de carga inicial, solo faltan los archivos blob

--- proyecto_final/scripts/s-09-carga-inicial.sql
+++ proyecto_final/scripts/s-09-carga-inicial.sql
@@ -8,18 +8,19 @@ prompt Cargando datos iniciales
 set define off
 
 prompt descargando archivos de carga inicial
-@s-09-descarga-archivos-carga-inicial.sql
+-- INVOCAR EL SIGUIENTE SCRIPT SOLO SI NO SE CUENTA CON LOS SCRIPTS DE CARGA INICIAL
+--@s-09-descarga-archivos-carga-inicial.sql
 
 prompt Deshabilitando constraints de entidades blob
-@s-09-deshabilitar-contraints.sql
+@s-09-deshabilitar-constraints.sql
 
 -- cargador 1
 prompt Cargando datos de vivienda
 @carga-inicial/s-09-vivienda.sql
 -- cargador 2
---@carga-inicial/s-09-vivienda_renta.sql
+@carga-inicial/s-09-vivienda_renta.sql
 -- cargador 3
---@carga-inicial/s-09-vivienda_vacacional.sql
+@carga-inicial/s-09-vivienda_vacacional.sql
 -- cargador 4
 prompt Cargando datos de status vivienda
 @carga-inicial/s-09-status_vivienda.sql
@@ -33,7 +34,7 @@ prompt Cargando datos de imagen
 prompt Cargando datos de servicio
 @carga-inicial/s-09-servicio.sql
 -- cargador 8
---@carga-inicial/s-09-servicio_vivienda.sql -- PROBLEMAS CON UNIQUE
+@carga-inicial/s-09-servicio_vivienda.sql 
 -- cargador 9
 prompt Cargando datos de usuario
 @carga-inicial/s-09-usuario.sql
@@ -54,7 +55,7 @@ prompt Cargando datos de vivienda venta
 @carga-inicial/s-09-vivienda_venta.sql
 -- cargador 15
 prompt Cargando datos de pago vivienda
---@carga-inicial/s-09-pago_vivienda.sql --PROBLEMAS CON UNIQUE
+@carga-inicial/s-09-pago_vivienda.sql
 -- cargador 16
 prompt Cargando datos de alquiler
 @carga-inicial/s-09-alquiler.sql
@@ -70,8 +71,8 @@ set define on
 -- ANTES DE HABILITAR LOS CONTRAINTS DEBEMOS CARGAR TODOS LOS
 -- ARCHIVOS BLOB
 
-prompt Habilitando constraints de entidades blob
-@s-09-habilitar-contraints.sql
+--prompt Habilitando constraints de entidades blob
+--@s-09-habilitar-constraints.sql
 
 --Prompt confirmando cambios
 --commit;
--- proyecto_final/scripts/s-09-descarga-archivos-carga-inicial.sql
+++ proyecto_final/scripts/s-09-descarga-archivos-carga-inicial.sql
@@ -3,24 +3,24 @@
 --@Fecha creación: 30/11/2019
 --@Descripción: Descarga de archivos de carga inicial
 
--- !curl "https://api.mockaroo.com/api/f4c558f0?count=15&key=d52330b0" > carga-inicial/s-09-alquiler.sql
--- !curl "https://api.mockaroo.com/api/330b1720?count=50&key=d52330b0" > carga-inicial/s-09-clave_deposito.sql
--- !curl "https://api.mockaroo.com/api/87c2fe10?count=15&key=d52330b0" > carga-inicial/s-09-contrato.sql
--- !curl "https://api.mockaroo.com/api/3008fe40?count=100&key=d52330b0" > carga-inicial/s-09-historico_status_vivienda.sql
--- !curl "https://api.mockaroo.com/api/9936d880?count=30&key=d52330b0" > carga-inicial/s-09-interesado_vivienda_vac.sql
--- !curl "https://api.mockaroo.com/api/3f498cb0?count=50&key=d52330b0" > carga-inicial/s-09-mensaje.sql
--- !curl "https://api.mockaroo.com/api/487055a0?count=100&key=d52330b0" > carga-inicial/s-09-servicio_vivienda.sql --PROBLEMAS CON UNIQUE
--- !curl "https://api.mockaroo.com/api/edfece70?count=6&key=d52330b0" > carga-inicial/s-09-status_vivienda.sql
--- !curl "https://api.mockaroo.com/api/23b94b20?count=50&key=d52330b0" > carga-inicial/s-09-tarjeta.sql
--- !curl "https://api.mockaroo.com/api/72a75a40?count=100&key=d52330b0" > carga-inicial/s-09-usuario.sql
--- !curl "https://api.mockaroo.com/api/3c61fa90?count=100&key=d52330b0" > carga-inicial/s-09-vivienda.sql
--- !curl "https://api.mockaroo.com/api/1bcc0800?count=30&key=d52330b0" > carga-inicial/s-09-vivienda_renta.sql -- PROBLEMAS CON ID
--- !curl "https://api.mockaroo.com/api/7a1f8360?count=30&key=d52330b0" > carga-inicial/s-09-vivienda_renta_clave_dep.sql
--- !curl "https://api.mockaroo.com/api/721c2050?count=30&key=d52330b0" > carga-inicial/s-09-vivienda_vacacional.sql -- PROBLEMAS CON ID
+!curl "https://api.mockaroo.com/api/f4c558f0?count=15&key=d52330b0" > carga-inicial/s-09-alquiler.sql
+!curl "https://api.mockaroo.com/api/330b1720?count=50&key=d52330b0" > carga-inicial/s-09-clave_deposito.sql
+!curl "https://api.mockaroo.com/api/87c2fe10?count=15&key=d52330b0" > carga-inicial/s-09-contrato.sql
+!curl "https://api.mockaroo.com/api/3008fe40?count=100&key=d52330b0" > carga-inicial/s-09-historico_status_vivienda.sql
+!curl "https://api.mockaroo.com/api/9936d880?count=30&key=d52330b0" > carga-inicial/s-09-interesado_vivienda_vac.sql
+!curl "https://api.mockaroo.com/api/3f498cb0?count=50&key=d52330b0" > carga-inicial/s-09-mensaje.sql
+!curl "https://api.mockaroo.com/api/487055a0?count=100&key=d52330b0" > carga-inicial/s-09-servicio_vivienda.sql --PROBLEMAS CON UNIQUE
+!curl "https://api.mockaroo.com/api/edfece70?count=6&key=d52330b0" > carga-inicial/s-09-status_vivienda.sql
+!curl "https://api.mockaroo.com/api/23b94b20?count=50&key=d52330b0" > carga-inicial/s-09-tarjeta.sql
+!curl "https://api.mockaroo.com/api/72a75a40?count=100&key=d52330b0" > carga-inicial/s-09-usuario.sql
+!curl "https://api.mockaroo.com/api/3c61fa90?count=100&key=d52330b0" > carga-inicial/s-09-vivienda.sql
+!curl "https://api.mockaroo.com/api/1bcc0800?count=30&key=d52330b0" > carga-inicial/s-09-vivienda_renta.sql -- PROBLEMAS CON ID
+!curl "https://api.mockaroo.com/api/7a1f8360?count=30&key=d52330b0" > carga-inicial/s-09-vivienda_renta_clave_dep.sql
+!curl "https://api.mockaroo.com/api/721c2050?count=30&key=d52330b0" > carga-inicial/s-09-vivienda_vacacional.sql -- PROBLEMAS CON ID
 
--- Los que deberían tener blob
--- !curl "https://api.mockaroo.com/api/4ab3f5d0?count=150&key=d52330b0" > carga-inicial/s-09-pago_vivienda.sql --PROBLEMAS CON UNIQUE
--- !curl "https://api.mockaroo.com/api/1b2a0d60?count=15&key=d52330b0" > carga-inicial/s-09-servicio.sql
--- !curl "https://api.mockaroo.com/api/bb8db5b0?count=40&key=d52330b0" > carga-inicial/s-09-imagen.sql
--- !curl "https://api.mockaroo.com/api/b7414ef0?count=100&key=d52330b0" > carga-inicial/s-09-vivienda_venta.sql
--- !curl "https://api.mockaroo.com/api/87c2fe10?count=15&key=d52330b0" > carga-inicial/s-09-contrato.sql
+Los que deberían tener blob
+!curl "https://api.mockaroo.com/api/4ab3f5d0?count=150&key=d52330b0" > carga-inicial/s-09-pago_vivienda.sql --PROBLEMAS CON UNIQUE
+!curl "https://api.mockaroo.com/api/1b2a0d60?count=15&key=d52330b0" > carga-inicial/s-09-servicio.sql
+!curl "https://api.mockaroo.com/api/bb8db5b0?count=40&key=d52330b0" > carga-inicial/s-09-imagen.sql
+!curl "https://api.mockaroo.com/api/b7414ef0?count=100&key=d52330b0" > carga-inicial/s-09-vivienda_venta.sql
+!curl "https://api.mockaroo.com/api/87c2fe10?count=15&key=d52330b0" > carga-inicial/s-09-contrato.sql
