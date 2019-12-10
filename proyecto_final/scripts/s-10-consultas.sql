32cb46f23cbe04a999db0c0399650f8c75938084
KarinaFlo <karinaf141@gmail.com>
2019-12-10 17:17:43 -0600

    Agrego consulta funcional

--- proyecto_final/scripts/s-10-consultas.sql
+++ proyecto_final/scripts/s-10-consultas.sql
@@ -37,3 +37,39 @@ group by s.servicio_id)
 --group by s.servicio_id,s.nombre;
 
 
+--usuario que más contratos de renta haya tenido en los últimos 5 años.
+
+
+
+--Ganancias vendidas en los ultimos 3 años sin contar las ventas en los meses de diciembre
+select sum(vv.comision_publicidad) as ganancias_totales from (
+select vivienda_id,status_vivienda_id,fecha_status,es_venta
+from vivienda
+where es_venta=1 and status_vivienda_id=5 and fecha_status
+between (sysdate-1095) and sysdate
+minus
+select vivienda_id,status_vivienda_id,fecha_status,es_venta
+from vivienda
+where extract(month from to_date(fecha_status,'dd/mm/yyyy')=12)
+order by fecha_status) q1
+join vivienda_venta vv
+on q1.vivienda_id = vv.vivienda_id;
+
+
+--Funciona en mis datos de prueba usando tipo
+--select sum(vv.comision_publicidad)
+--from(select vivienda_id,tipo,status_vivienda_id,fecha_status
+--from vivienda 
+--where tipo='s' and status_vivienda_id=5 and
+--fecha_status between (sysdate-1095) and sysdate
+--minus 
+--select vivienda_id,tipo,status_vivienda_id,fecha_status
+--from vivienda 
+--where extract(month from
+--to_date(fecha_status,'dd/mm/yyyy'))=12
+--order by fecha_status) q1
+--join vivienda_venta vv
+--on q1.vivienda_id = vv.vivienda_id;
+
+
+
