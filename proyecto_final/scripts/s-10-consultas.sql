2f0229203e620fbe9f81c34c1281b0ea1649ac42
KarinaFlo <karinaf141@gmail.com>
2019-12-10 21:04:36 -0600

    Agrego consulta funcional

--- proyecto_final/scripts/s-10-consultas.sql
+++ proyecto_final/scripts/s-10-consultas.sql
@@ -4,6 +4,7 @@
 --@Descripción: Consultas
 
 --Mostrar los datos del servicio más común en las viviendas anunciadas. (Uso de join, funciones de agregación, subconsulta).
+--FUNCIONA
 select servicio_id,nombre,descripcion,total_viviendas
 from (
 select s.servicio_id,s.nombre,s.descripcion,count(sv.vivienda_id) as total_viviendas
@@ -38,10 +39,33 @@ group by s.servicio_id)
 
 
 --usuario que más contratos de renta haya tenido en los últimos 5 años.
+--FUNCIONA
+
+select * from v_usuario
+where usuario_id = (
+select usuario_id from(
+select usuario_id,total_contratos
+from(
+select u.usuario_id,u.tipo,count(c.contrato_id) as total_contratos
+from usuario u 
+join contrato c
+on u.usuario_id=c.usuario_id
+group by u.usuario_id,u.tipo
+having u.tipo='C')
+where total_contratos = (
+select max(total_contratos) from(
+select u.usuario_id,u.tipo,count(c.contrato_id) as total_contratos
+from usuario u 
+join contrato c
+on u.usuario_id=c.usuario_id
+group by u.usuario_id,u.tipo
+having u.tipo='C'))));
+
 
 
 
 --Ganancias vendidas en los ultimos 3 años sin contar las ventas en los meses de diciembre
+--FUNCIONA 
 select sum(vv.comision_publicidad) as ganancias_totales from (
 select vivienda_id,status_vivienda_id,fecha_status,es_venta
 from vivienda
@@ -50,26 +74,12 @@ between (sysdate-1095) and sysdate
 minus
 select vivienda_id,status_vivienda_id,fecha_status,es_venta
 from vivienda
-where extract(month from to_date(fecha_status,'dd/mm/yyyy')=12)
-order by fecha_status) q1
+where extract(month from to_date(fecha_status,'dd/mm/yyyy'))=12
+order by fecha_status
+)q1
 join vivienda_venta vv
 on q1.vivienda_id = vv.vivienda_id;
 
-
---Funciona en mis datos de prueba usando tipo
---select sum(vv.comision_publicidad)
---from(select vivienda_id,tipo,status_vivienda_id,fecha_status
---from vivienda 
---where tipo='s' and status_vivienda_id=5 and
---fecha_status between (sysdate-1095) and sysdate
---minus 
---select vivienda_id,tipo,status_vivienda_id,fecha_status
---from vivienda 
---where extract(month from
---to_date(fecha_status,'dd/mm/yyyy'))=12
---order by fecha_status) q1
---join vivienda_venta vv
---on q1.vivienda_id = vv.vivienda_id;
 
 
 
